b075d90f966641c823110cd52c98286759437512
skyworldy <natthapol.forwork@gmail.com>
2021-10-28 20:16:52 +0700

    Update!!

---
+++ app/src/main/java/com/example/projectcss3211/SearchMain.java
@@ -0,0 +1,22 @@
+package com.example.projectcss3211;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONString;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class SearchMain {
+
+    ArrayList<String> ingredientTags = new ArrayList<String>();
+    ArrayList<String> cuisineTags = new ArrayList<String>();
+
+    public SearchMain(){
+    }
+    public static void readJSON(){
+        System.out.println("eieiei");
+    }
+}
---
+++ app/src/main/java/com/example/projectcss3211/Test.java
@@ -0,0 +1,7 @@
+package com.example.projectcss3211;
+
+public class Test {
+    public static void main(String[] args){
+        SearchMain.readJSON();
+    }
+}
